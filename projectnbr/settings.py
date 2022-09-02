6dde9259f90c57cd4202ca09189b8a87c3cbbb4f
Akassh Nade <asky.nade@gmail.com>
2022-09-02 12:04:25 +0530

    changed Requirment.txt

--- projectnbr/settings.py
+++ projectnbr/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q^pxbf@cx6#4ql0!6#@&kl636nau69xmniyu-4rkoj95443(^x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['localhost','projectnbr.herokuapp.com']
 
 
 # Application definition
@@ -193,8 +193,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT=BASE_DIR / 'static'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
